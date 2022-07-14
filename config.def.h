0a37471fed557793b6ccc67235ceb60f7e126b8a
1337h4x0r <1337h4x0r@gmx.de>
2022-07-15 00:01:18 +0200

    apply "attachbottom" patch

--- config.def.h
+++ config.def.h
@@ -78,7 +78,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = -1 } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+/*	{ MODKEY,                       XK_Return, zoom,           {0} },*/
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ ALTKEY,                       XK_F4,     killclient,     {0} },
 	{ MODKEY,                       XK_c,      killclient,     {0} },
