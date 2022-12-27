8d084073876bff3352fdfc0c07f4b3ac2b127be0
1337h4x0r <1337h4x0r@gmx.de>
2022-12-27 02:18:14 +0100

    launch terminal with background_opacity parameter

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "terminal", NULL };
+static const char *termcmd[]  = { "terminal", "-o", "background_opacity=0.8", NULL };
 static const char *explorercmd[] = { "explorer", NULL };
 
 #include "movestack.c"
