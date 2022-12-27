5e75a2ee6ef18c3fb97329d7034b4c7aa6ef2c55
1337h4x0r <1337h4x0r@gmx.de>
2022-12-27 02:15:02 +0100

    configure default opacity

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const int showbar            = 1;        /* 0 means no standard bar */
 static const int topbar             = 1;        /* 0 means standard bar at bottom */
 static const int extrabar           = 1;        /* 0 means no extra bar */
 static const char statussep         = ';';      /* separator between statuses */
-static const double defaultopacity  = 0.75;
+static const double defaultopacity  = 1.0;      /* opacity all windows are initially started with */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
