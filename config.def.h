9d731cd778f64dd25a4959ec22df38f196561ef7
1337h4x0r <1337h4x0r@gmx.de>
2023-01-03 21:06:50 +0100

    set Pavucontrol floating

--- config.def.h
+++ config.def.h
@@ -37,6 +37,7 @@ static const Rule rules[] = {
 	/* { "Gimp",         NULL,    NULL,    0,          1,         1.0,              -1 }, */
 	/* { "Firefox",      NULL,    NULL,    1 << 8,     0,         1.0,              -1 }, */
 	   { "St",           NULL,    NULL,    0,          0,         defaultopacity,   -1 },
+	   { "Pavucontrol",  NULL,    NULL,    0,          1,         1.0,              -1 }
 };
 
 /* layout(s) */
