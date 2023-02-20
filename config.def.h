123081b15aba1a4963cebb739afecd65f7be2920
1337h4x0r <1337h4x0r@gmx.de>
2023-02-20 01:34:33 +0100

    change default border color

--- config.def.h
+++ config.def.h
@@ -12,6 +12,7 @@ static const char statussep         = ';';      /* separator between statuses */
 static const double defaultopacity  = 1.0;      /* opacity all windows are initially started with */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -19,7 +20,7 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_black },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
 };
